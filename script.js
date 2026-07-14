e24c2869b6ae0a255e2f28cdf9c08243fb5a6df8
faizan-builds-coder <faizanalishahzad35@gmail.com>
2026-07-14 13:08:15 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+//------------------------------ Task 1:
+
+console.log("Task1");
+
+console.log("Hello Beta Communes");
+console.log("")
+
+//------------------------------ Task 2:
+console.log("Task2");
+
+let name = "Faizan Ali";
+let age = 20;
+let city = "Lahore";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("City:", city);
+console.log("")
+
+//------------------------------ Task 3:
+console.log("Task3");
+
+let num1 = 10;
+let num2 = 30;
+console.log("Addition:", num1 + num2);
+console.log("Subtraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division:", num1 / num2);
+console.log("")
+
+//------------------------------ Task 4:
+console.log("Task4");
+
+let celsius = 30;
+let fahrenheit = (celsius * 9 / 5) + 32;
+
+console.log("Celsius:", celsius);
+console.log("Fahrenheit:", fahrenheit);
+console.log("")
+
+//------------------------------ Task 5:
+console.log("Task5");
+
+let english = 85;
+let math = 90;
+let science = 88;
+let computer = 95;
+let urdu = 80;
+
+let totalobtainedMarks = english + math + science + computer + urdu;
+let totalMarks = 500;
+
+let percentage = (totalobtainedMarks /totalMarks) * 100;
+
+console.log("English Marks:", english);
+console.log("Math Marks:", math);
+console.log("Science Marks:", science);
+console.log("Computer Marks:", computer);
+console.log("Urdu Marks:", urdu);
+
+console.log("Total Obtained Marks:", totalobtainedMarks);
+console.log("Percentage:", percentage + "%");
+console.log("")
